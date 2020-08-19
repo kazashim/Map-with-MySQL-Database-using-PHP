3902ff4772c5096ccfb82211d81473159093d4c9
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-19 16:08:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,13 @@
 |Email: Kazashim@cynojine.com
 |Website: https://cynojine.com
 */
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'location_db';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+ $coordinates = array();
+ $latitudes = array();
+ $longitudes = array();
