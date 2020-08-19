62d2d2d71edb5b3df6bde1d3bf630527d719ba92
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-19 16:03:14 +0200

    update

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+/*
+|Author @ Kazashim
+|
+|
+*/
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "";
+	
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
