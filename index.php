9c3c7e9cb0f2c9800de4a2ca33a4aa2399405e90
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-19 16:11:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -30,3 +30,28 @@ $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
  $lastcount = count($coordinates)-1;
  $coordinates[$lastcount] = trim($coordinates[$lastcount], ",");	
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Maps</title>
+	</head>
+
+	<body>
+	    <nav>  
+			<ul> 
+				<li class="active"><a href="#"><img src="img/map.png"></a></li>
+				<li><a href="#"><img src="img/logout.png"></a></li>
+			</ul> 
+		</nav>
+
+		 <div class="outer-scontainer">
+	        <div class="row">
+	            <form class="form-horizontal" action="" method="post" name="frmCSVImport" id="frmCSVImport" enctype="multipart/form-data">
+	            	<div class="form-area">	      
+    					<button type="submit" id="submit" name="import" class="btn-submit">RELOAD DATA</button><br />
+					</div>
+	            </form>
+	        </div>
