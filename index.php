b350ef4d1dd972190718703bf795e29a432996ed
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-19 16:09:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,3 +14,14 @@ $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
  $coordinates = array();
  $latitudes = array();
  $longitudes = array();
+
+ // Select all the rows in the markers table
+ $query = "SELECT  `locationLatitude`, `locationLongitude` FROM `location_tab` ";
+ $result = $mysqli->query($query) or die('data selection for google map failed: ' . $mysqli->error);
+
+  while ($row = mysqli_fetch_array($result)) {
+
+     $latitudes[] = $row['locationLatitude'];
+     $longitudes[] = $row['locationLongitude'];
+     $coordinates[] = 'new google.maps.LatLng(' . $row['locationLatitude'] .','. $row['locationLongitude'] .'),';
+ }
